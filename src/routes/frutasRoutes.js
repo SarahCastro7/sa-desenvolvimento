b046a1c50c8077c7324a5738977e1d1d7b73c5f3
sarah! <sarah.c.nascimento10@gmail.com>
2026-05-19 14:09:45 -0300

    19-05
    precisa arrumar o routes pq nao esta querendo importar o service. Em breve quero mudar o banco de dados, começa a revisar react e tailwind css para adicionar na SA, alem que precisa revisar os arquivos de routes e service, porq ambos contem errossssssss

--- src/routes/frutasRoutes.js
+++ src/routes/frutasRoutes.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { frutasService } from "../services/frutasServices.js";
+import express from 'express';
+import { frutasService } from './service/frutasServices.js';
 
 const routeFrutas = express.Router();
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routeFrutas from "./routes/frutasRoutes.js";
+import express from 'express';
+import routeFrutas from './routes/frutasRoutes.js';
 
 //mais para frente precisa fazer o html e css, nao vamos usar html puro e sim com react e a biblioteca tailwind
 
