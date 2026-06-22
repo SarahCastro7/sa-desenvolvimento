20df9361db5ae6a4e4d5d7dda5ff7d57862f60f7
sarah! <sarah.c.nascimento10@gmail.com>
2026-06-22 17:15:03 -0300

    22-6
    UMA BOA PARTE DAS TABELAS ESTAO CORRETAS

--- backend/scripts.sql
+++ backend/scripts.sql
@@ -1,32 +1,30 @@
 --as tabelas ficam aqui, para facilictar o acesso delas e as suas aplicações
-BEGIN;
-
-
 CREATE TABLE IF NOT EXISTS public.categorias
 (
-    id_categotia uuid NOT NULL DEFAULT uuid_generate_v4(),
+    id_categoria uuid NOT NULL DEFAULT uuid_generate_v4(),
     nome_categoria text COLLATE pg_catalog."default" NOT NULL,
     lote_categoria integer NOT NULL,
     setor_categoria character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT categorias_pkey PRIMARY KEY (id_categotia)
+    CONSTRAINT categorias_pkey PRIMARY KEY (id_categoria)
 );
 
 CREATE TABLE IF NOT EXISTS public.equipamentos
 (
     id_equipamento uuid NOT NULL DEFAULT uuid_generate_v4(),
     nome_equipamento text COLLATE pg_catalog."default" NOT NULL,
-    quantidd_equipamento integer NOT NULL,
-    equipamento_categoria character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT equipamentos_pkey PRIMARY KEY (id_equipamento, nome_equipamento, quantidd_equipamento)
+    quantidade_equipamento integer NOT NULL,
+    id_categoria uuid NOT NULL,
+    CONSTRAINT equipamentos_pkey PRIMARY KEY (id_equipamento)
 );
 
 CREATE TABLE IF NOT EXISTS public.funcionarios
 (
-    id_funcionario uuid NOT NULL,
+    id_funcionario uuid NOT NULL DEFAULT uuid_generate_v4(),
     nome_funcionario character varying(100) COLLATE pg_catalog."default" NOT NULL,
     email character varying(100) COLLATE pg_catalog."default" NOT NULL,
     cargo character varying(50) COLLATE pg_catalog."default" NOT NULL,
     setor_funcionario character varying COLLATE pg_catalog."default" NOT NULL,
+    id_equipamento uuid NOT NULL,
     CONSTRAINT funcionarios_pkey PRIMARY KEY (id_funcionario)
 );
 
@@ -38,10 +36,22 @@ CREATE TABLE IF NOT EXISTS public.setores
     CONSTRAINT setor_pkey PRIMARY KEY (id_setor)
 );
 
+ALTER TABLE IF EXISTS public.equipamentos
+    ADD CONSTRAINT fk_equipamento_categoria FOREIGN KEY (id_categoria)
+    REFERENCES public.categorias (id_categoria) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+
+
+ALTER TABLE IF EXISTS public.funcionarios
+    ADD CONSTRAINT fk_funcionario_equipamento FOREIGN KEY (id_equipamento)
+    REFERENCES public.equipamentos (id_equipamento) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
+
+
 ALTER TABLE IF EXISTS public.setores
     ADD CONSTRAINT fk_setor_categoria FOREIGN KEY (categoria_setor)
-    REFERENCES public.categorias (id_categotia) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION;
-
-END;
+    REFERENCES public.categorias (id_categoria) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT;
