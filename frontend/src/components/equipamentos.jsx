5cad9847c7a31d3184592d76c12f322d7a86c676
sarah! <sarah.c.nascimento10@gmail.com>
2026-07-13 14:58:02 -0300

    fix: corrigido frontend com problemas, estruturação de pastas e arquivos

---
+++ frontend/src/components/equipamentos.jsx
@@ -0,0 +1,9 @@
+//vamos usar ess arquivo para o Frontend consumir o Backend
+
+import { useState, useEffect } from 'react';
+import Equipamentos from './components/equipamentos.jsx';
+
+function Equipamentos() {
+    const [equipamentos, setEquipamentos] = useState([]);
+
+}
