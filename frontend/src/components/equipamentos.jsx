51011bcb1595c30e83ed097f52cd778501a497ac
sarah! <sarah.c.nascimento10@gmail.com>
2026-07-13 15:01:39 -0300

    13-07
    agora as pastas, e estruturas do projeto esta melhor, hoje eu preciso arrumar os card e a carinha do site

--- frontend/src/components/equipamentos.jsx
+++ frontend/src/components/equipamentos.jsx
@@ -1,9 +1,9 @@
 //vamos usar ess arquivo para o Frontend consumir o Backend
 
-import { useState, useEffect } from 'react';
-import Equipamentos from './components/equipamentos.jsx';
-
-function Equipamentos() {
-    const [equipamentos, setEquipamentos] = useState([]);
-
+export default function Equipamentos({data}) {
+return (
+    <div>
+        
+    </div>
+)
 }
