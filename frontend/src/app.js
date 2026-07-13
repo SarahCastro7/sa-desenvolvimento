be759b0ab56acec4263919cb98c96c1a7c7b73ef
sarah! <sarah.c.nascimento10@gmail.com>
2026-07-13 17:11:21 -0300

    13-07
    arrumando mais ume negocioss

---
+++ frontend/src/app.js
@@ -0,0 +1,25 @@
+export async function cadastrarEquipamento(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    const nome = formData.get("nome");
+    console.log("nome do equipamento", nome);
+
+
+    try {
+        const res = await fetch("http://localhost:3000/frutas", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nome })
+        })
+        console.log(res);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
--- frontend/src/components/equipamentos.jsx
+++
@@ -1,8 +0,0 @@
-//vamos usar esse arquivo para o Frontend consumir o Backend
-export default function Equipamentos({data}) {
-return (
-    <div>
-        
-    </div>
-)
-}
