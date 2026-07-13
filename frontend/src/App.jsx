46707a0adc96170eac788ffe31b1618ef2a6d832
sarah! <sarah.c.nascimento10@gmail.com>
2026-07-13 16:00:13 -0300

    13-07
    certa parte esta funcionando, mais falta adicionar os card e organizar a front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-
-import './styles/App.css'
+import { useEffect, useState } from 'react';
+import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [equipamentos, setEquipamentos] = useState([]);
+
+  useEffect(() => {
+    loadEquipamentos();
+  }, []);
+
+  async function loadEquipamentos() {
+    try {
+      const res = await fetch('http://localhost:3000/equipamentos');
+      const data = await res.json();
+      setEquipamentos(data);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <h1>Equipamentos</h1>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/equipamentos.jsx
+++ frontend/src/components/equipamentos.jsx
@@ -1,5 +1,4 @@
-//vamos usar ess arquivo para o Frontend consumir o Backend
-
+//vamos usar esse arquivo para o Frontend consumir o Backend
 export default function Equipamentos({data}) {
 return (
     <div>
